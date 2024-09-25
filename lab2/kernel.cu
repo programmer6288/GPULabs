4dfcf8b1bdec21ac6128e09fe38cbbaaf883571e
Aditya Kaushik <adityakaushik314@gmail.com>
2024-09-25 17:22:02 -0400

    fixing shared mem

--- lab2/kernel.cu
+++ lab2/kernel.cu
@@ -239,26 +239,24 @@ int main(int argc, char* argv[]) {
 
     int logsize = (int) (log2(BUFSIZE));
     bitonic_sort_shared<<<modSize / BUFSIZE, BUFSIZE>>>(gpuArr, logsize, logsize);
-    for (int i = logsize + 1; i <= log2(modSize); i++) {
-        for (int j = i - 1; j >= logsize; j--) {
-            if (j == logsize) {
-                bitonic_sort_shared<<<modSize / BUFSIZE, BUFSIZE>>>(gpuArr, logsize, i); 
+    // for (int i = logsize + 1; i <= log2(modSize); i++) {
+    //     for (int j = i - 1; j >= logsize; j--) {
+    //         if (j == logsize) {
+    //             bitonic_sort_shared<<<modSize / BUFSIZE, BUFSIZE>>>(gpuArr, logsize, i); 
+    //         } else {
+    //             bitonic_sort<<<(modSize) / BUFSIZE, BUFSIZE / 2>>>(gpuArr, i, j);
+    //         }
+    //     }
+    // }
+    for (int i = 1; i <= log2(modSize); i++) {
+        for (int j = i - 1; j >= 0; j--) {
+            if ((1 << j) < BUFSIZE) {
+                bitonic_sort_shared_merge<<<(modSize + BUFSIZE - 1) / BUFSIZE, BUFSIZE / 2>>>(gpuArr, i, j);
             } else {
-                bitonic_sort<<<(modSize) / BUFSIZE, BUFSIZE / 2>>>(gpuArr, i, j);
+                bitonic_sort<<<(modSize + BUFSIZE - 1) / BUFSIZE, BUFSIZE / 2>>>(gpuArr, i, j);
             }
         }
     }
-//     for (int i = 1; i <= log2(modSize); i++) {
-//         for (int j = i - 1; j >= 0; j--) {
-// 	    if ((1 << j) < BUFSIZE) {
-// 		bitonic_sort_shared_merge<<<(modSize + BUFSIZE - 1) / BUFSIZE, BUFSIZE / 2>>>(gpuArr, i, j);
-// 	    } else {
-// 		bitonic_sort<<<(modSize + BUFSIZE - 1) / BUFSIZE, BUFSIZE / 2>>>(gpuArr, i, j);
-// 	    }
-// //	    cudaMemcpy(arrSortedGpu, gpuArr + (modSize - size), size * sizeof(int), cudaMemcpyDeviceToHost);
-// //	    for (int i = 0; i < size; i++) printf("arr[%d] = %d\n", i, arrSortedGpu[i]);
-//         }
-//     }
 
     cudaEventRecord(stop);
     cudaEventSynchronize(stop);
